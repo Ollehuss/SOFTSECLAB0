57d09ac4ec10068a3ea2bf6543c5384bd0c45b84
idaslanjankic <idaslanjankic@gmail.com>
2026-09-07 11:11:39 +0200

    support personal name

--- src/api.py
+++ src/api.py
@@ -1,6 +1,9 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 @app.route("/hello", methods=["GET"])
 def hello_world():
+    name = request.args.get("name")
+    if name:
+        return jsonify({"message": f"Hello, {name}!"})
     return jsonify({"message": "Hello, World!"})

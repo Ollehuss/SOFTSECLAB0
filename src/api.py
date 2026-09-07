9860bea4a98e8c394af39f1ebba82f34911e6746
Ollehuss <127675229+Ollehuss@users.noreply.github.com>
2026-09-07 19:58:34 +0200

    s
    d

---
+++ src/api.py
@@ -0,0 +1,12 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.get("/hello")
+def hello():
+    return jsonify({"message": "Hello, world!"})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
